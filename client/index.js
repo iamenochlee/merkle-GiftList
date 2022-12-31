497eac5dfd4f3e786711fef9ca40764560df8a20
enochlee <enochakinbode466@gmail.com>
2022-12-31 16:09:22 +0100

    dev: added proof functionality

--- client/index.js
+++ client/index.js
@@ -1,17 +1,21 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const fs = require("fs");
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
+const merkleTree = new MerkleTree(niceList);
+fs.writeFileSync("../server/root.json", JSON.stringify(merkleTree.getRoot()));
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
-
+  //input name in string when running the command, can also replace
+  const name = process.argv[2];
+  const index = niceList.indexOf(name);
+  const proof = merkleTree.getProof(index);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name,
+    proof,
   });
-
   console.log({ gift });
 }
-
 main();
--- server/index.js
+++ server/index.js
@@ -1,25 +1,21 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const fs = require("fs");
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = JSON.stringify(fs.readFileSync("./root.json"));
+console.log(MERKLE_ROOT);
 
-app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
-
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
+app.post("/gift", (req, res) => {
+  const { name, proof } = req.body;
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if (isInTheList) {
+    res.send("Merry Christmas!");
+  } else {
     res.send("You are not on the list :(");
   }
 });
